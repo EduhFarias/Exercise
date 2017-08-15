09085329d086cc4f5d30de5b5fa6e988da4e9116
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-08-15 20:39:08 -0300

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -10,14 +10,14 @@
  */
 typedef struct node{
     int item;
-    int *next;
+    struct node *next;
 }Node;
 Node* createLinkedList();
 Node* insertNode(Node *node, int item);
-void printLinkedList(Node *first);
-int isEmpty(Node *first);
-Node* search(Node *first, int item);
-Node* removeNode(Node *first, int item);
+void printLinkedList(Node *head);
+int isEmpty(Node *head);
+Node* search(Node *head, int item);
+Node* removeNode(Node *head, int item);
 //Função que verifica se a lista B é um subconjunto da lista A
 void check(Node *A, Node *B);
 #endif //EXERCISE_LINKEDLIST_H

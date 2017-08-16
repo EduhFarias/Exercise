3492d86d8e3aba89ea65cf72e95173a83044ac0e
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-08-16 20:01:47 -0300

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -8,16 +8,25 @@
     LinkedList.h
     TAD para representar uma Lista Encadeada.
  */
+
 typedef struct node{
     int item;
     struct node *next;
 }Node;
+
 Node* createLinkedList();
+
 Node* insertNode(Node *node, int item);
+
 void printLinkedList(Node *head);
+
 int isEmpty(Node *head);
+
 Node* search(Node *head, int item);
+
 Node* removeNode(Node *head, int item);
+
 //Função que verifica se a lista B é um subconjunto da lista A
 void check(Node *A, Node *B);
+
 #endif //EXERCISE_LINKEDLIST_H

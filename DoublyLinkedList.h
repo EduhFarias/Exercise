312fecf99fe0aa3519ac8118a72a4a3e9f2b1c7e
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-08-16 20:01:24 -0300

    Update DoublyLinkedList.h

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -8,17 +8,25 @@
     DoublyLinkedList.h
     TAD para representar uma Lista Duplamente Encadeada.
  */
+
 typedef struct node{
     int item;
     struct node *next;
     struct node *previous;
 }Node;
+
 Node* createDoublyLinkedList();
+
 Node* insertNode(Node *head, int item);
+
 void printDoublyLinkedList_Foward(Node *head);
+
 void printDoublyLinkedList_Backward(Node *tail);
+
 int isEmpty(Node *head);
+
 Node* search(Node *head, int item);
+
 Node* removeNode(Node *head, int item);
 
 #endif //EXERCISE_DOUBLYLINKEDLIST_H

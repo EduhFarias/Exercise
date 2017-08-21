12b0625e1bffdbd182170a70186f94289d8e7a87
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-08-21 20:40:04 -0300

    Create PriorityQueue.h

---
+++ PriorityQueue.h
@@ -0,0 +1,29 @@
+//
+// Created by Eduardo on 21/08/2017.
+//
+
+
+#ifndef EXERCISE_PRIORITYQUEUE_H
+#define EXERCISE_PRIORITYQUEUE_H
+/*
+    PriorityQueue.h
+    TAD para representar uma Fila de Prioridade.
+ */
+
+typedef struct node Node;
+
+typedef struct queue PriorityQueue;
+ 
+PriorityQueue* createPriorityQueue();
+
+void enqueue(PriorityQueue *Priority_Queue, int a, int b);
+
+node* dequeue(PriorityQueue *Priority_Queue);
+
+int isEmpty(PriorityQueue *Priority_Queue);
+
+int maximum(PriorityQueue *Priority_Queue);
+
+int printPriorityQueue(PriorityQueue *Priority_Queue);
+
+#endif //EXERCISE_PRIORITYQUEUE_H

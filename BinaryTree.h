076f6db1ee8a15ef3d160038464e467f911a2c19
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-08-30 20:17:05 -0300

    Create BinaryTree.h

---
+++ BinaryTree.h
@@ -0,0 +1,30 @@
+//
+// Created by Eduardo on 30/08/2017.
+//
+
+#ifndef EXERCISE_BINARYTREE_H
+#define EXERCISE_BINARYTREE_H
+/*
+    BinaryTree.h
+    TAD para representar uma Árvore binária.
+ */
+
+typedef struct binary_tree BinaryTree;
+
+BinaryTree* createEmptyBinaryTree();
+
+BinaryTree* createBinaryTree(int item, BinaryTree *left, BinaryTree *right);
+
+BinaryTree* search(BinaryTree *bt, int item);
+
+void printInOrder(BinaryTree *bt);
+
+void printPreOrder(BinaryTree *bt);
+
+void printPostOrder(BinaryTree *bt);
+
+int isEmpty(BinaryTree *bt);
+
+BinaryTree* remove(BinaryTree *bt, int item);
+
+#endif //EXERCISE_BINARYTREE_H

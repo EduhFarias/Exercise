045ef7eb8bc3b0c81b2c1abfb580e2f464f66fe6
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-09-10 20:25:25 -0300

    Create Graph.h

---
+++ Graph.h
@@ -0,0 +1,25 @@
+//
+// Created by Eduardo on 10/09/2017.
+//
+
+#ifndef EXERCISE_GRAPH_H
+#define EXERCISE_GRAPH_H
+/*
+    Graph.h
+    TAD para representar um Grafo.
+ */
+
+typedef struct adj_list AdjList;
+typedef struct graph Graph;
+
+Graph* createGraph();
+
+void add_edge(Graph *graph, int vertex1, int vertex2);
+
+void dfs(Graph *graph, int source);
+
+void bfs(Graph *graph, int source);
+
+void printGraph(Graph *graph);
+
+#endif //EXERCISE_GRAPH_H

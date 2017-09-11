7246045832363bc03512a093821c0d225b280f80
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-09-10 22:25:55 -0300

    Create Heap.h

---
+++ Heap.h
@@ -0,0 +1,34 @@
+//
+// Created by Eduardo on 10/09/2017.
+//
+
+#ifndef EXERCISE_HEAP_H
+#define EXERCISE_HEAP_H
+/*
+    Heap.h
+    TAD para representar uma Heap.
+ */
+
+typedef struct heap Heap;
+
+Heap* createHeap();
+
+void* enqueue(Heap *heap, int item);
+
+int dequeue(Heap *heap);
+
+int getParentIndex(Heap *heap);
+
+int getLeftIndex(Heap *heap);
+
+int getRightIndex(Heap *heap);
+
+void max_heapify(Heap *heap, int item);
+
+void min_heapify(Heap *heap, int item);
+
+int item_of(Heap *heap, int item);
+
+void heapsort(Heap *heap);
+
+#endif //EXERCISE_HEAP_H

1b7e334a340b9af3cb0bb1b56eb6ed2c96214c3a
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-12-03 18:04:48 -0200

    Create Test.vhd

---
+++ Test.vhd
@@ -0,0 +1,21 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL
+
+entity test is
+  port(
+    clk : in std_logic;
+  
+  
+    o : out std_logic_vector : (7 downto 0)
+  );
+end test;  
+
+architecture arch of test is
+  -- uso de signal ou constant
+  begin
+  
+  --uso de when else, ou uso de with ... select /n ... <= with "..."
+  
+end arch;
+  
+  

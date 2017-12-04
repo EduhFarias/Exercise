ff82d341970f4c5bfd986c1a7e5c1adb3b968040
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2017-12-04 19:08:30 -0200

    Update Test.vhd

--- Test.vhd
+++ Test.vhd
@@ -5,21 +5,30 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY COMPARATOR IS
 	PORT(
-		A : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-    B : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		A, B : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
     
-    O : OUT STD_LOGIC
+    	EQUAL, DIFFER : OUT STD_LOGIC
 	);
-END TEST;
+END COMPARATOR;
 
 ARCHITECTURE ARCH OF COMPARATOR IS 
 	--
 	BEGIN
-	
-	
-	
-END ARCH;
-  
+	PROCESS(A,B);
+	BEGIN
+		IF (A > B) THEN
+			DIFFER <= '1';
+			EQUAL <= '0';
+		ELSIF (A < B) THEN
+			DIFFER <= '1';
+			EQUAL <= '0';
+		ELSE
+			EQUAL <= '1';
+			DIFFER <= '0';
+		END IF
+	END PROCESS;
+END ARCH;  
   
 http://vhdlguru.blogspot.com.br/2010/03/vhdl-code-for-bcd-to-7-segment-display.html
 https://stackoverflow.com/questions/21508949/bcd-to-7-segment-decoder
+https://www2.pcs.usp.br/~labdig/pdffiles_2012/contadoresVHDL.pdf
